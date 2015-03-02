c70650d6a2b70d4a90c37497a728d7e42a4e2e5f
Sam Whitney <sam@samwhitney.com>
2015-03-02 10:34:47 -0600

    begin implementing device authentication functionality

---
+++ routes/devices.js
@@ -0,0 +1,12 @@
+/*
+ * File: devices.js
+ * Authors: Sam Whitney, Aleks Sverdlovs
+ * Project: Chromocam web API
+ */
+
+ var db = require('../database');
+
+ /* TO DO
+  * -get hashed+salted password
+  * -return token (unique hash) for device to use for future API calls
+  */
